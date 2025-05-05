7648342824eaefda0e726e3795becd85eebc1cd8
luizlealdev <luizandreleal100@gmail.com>
2025-05-05 19:02:51 -0300

    feat: new icons

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //output: "export", //isso serve para gerar páginas estáticas
   eslint: {
     ignoreDuringBuilds: true
   },
